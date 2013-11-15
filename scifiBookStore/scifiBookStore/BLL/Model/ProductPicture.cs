e44ce27414ec04ac81841d78c9604c965e0623ad
RamonaWard <rward02@g.uafs.edu>
2013-11-15 09:57:20 -0600

    BLL layer

---
+++ scifiBookStore/scifiBookStore/BLL/Model/ProductPicture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace scifiBookStore.BLL.Model
+{
+    public class ProductPicture
+    {
+        public int PictureID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+
+        public string FileLocation { get; set; }
+        public string ThumbFileLocation { get; set; }
+
+        public bool USeAsLogoPicture { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string BackgroundColor { get; set; }
+
+        //Relationship properties
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
